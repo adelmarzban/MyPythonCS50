432d49aa72d7c8bc4319cb67905250492db40a1d
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-09-17 08:48:53 +0000

    Sat, Sep 17, 2022, 8:48 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,5 @@
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+if question == "42" | "Forty Two" | "forty-two":
+    print ("Yes")
+    else: print ("No")
+    
