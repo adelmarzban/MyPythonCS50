e1c26b4df2e522039c0f66c34df3d4954536ee4d
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-09-17 11:42:37 +0000

    Sat, Sep 17, 2022, 11:42 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 question = (input("What is the Answer to the Great Question of Life, the Universe, and Everything?"))
-if question.strip() == "42" or question.strip().lower()== "Forty Two" or question.strip().lower()== "forty-two" :
+if question.strip().lower() == "42" or question.strip().lower()== "Forty Two" or question.strip().lower()== "forty-two" or question.strip().lower()== "forty two":
     print("Yes")
 else:
     print("No")
