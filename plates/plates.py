83b59cd7984b3a12d1251fea65cb62f706493b92
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-10-23 17:53:32 +0000

    Sun, Oct 23, 2022, 5:53 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,9 +1,10 @@
+sym=[ ". ",", ", ";",":" ]
 plate= input("Input: ")
 if len(plate)<=6 and len(plate)>=2 \
             and plate[0].isalpha() \
             and plate[1].isalpha() \
-            and plate[-1].isdigit() \
-            and plate[0] !="0":
-        print("Valid")
+            and plate[-4::-1].isalpha() \
+            and plate[0] !="0"  :
+    print("Valid")
 else:
     print("Invalid")
