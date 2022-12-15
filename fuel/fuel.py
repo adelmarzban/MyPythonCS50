d0600df3a7229682e7e2a16c48e965cd40b77f2d
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-15 15:48:35 +0000

    Thu, Dec 15, 2022, 3:48 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,3 +1,13 @@
-a1 = "1/4"
-a2 = a1.split("/")
-print(a2)
+try:
+    Fraction = input("Fraction=")
+    Fraction2 = Fraction.split("/")
+
+    if (int(Fraction2[0])/int(Fraction2[1]))*100 == int("100"):
+        print ("F")
+    elif (int(Fraction2[0])/int(Fraction2[1]))*100 == int("0"):
+        print ("E")
+    else: print((int(Fraction2[0]) / int(Fraction2[1])) * 100 )
+
+
+except :
+    print("")
