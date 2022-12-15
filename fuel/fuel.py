9501359fd0b9f58f11d2d00c90e1cb8695df3f6c
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-15 20:01:28 +0000

    Thu, Dec 15, 2022, 8:01 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,6 +1,6 @@
 try:
     Fraction = input("Fraction=")
-    x , y = Fraction.split("/")
+    Fraction2 = Fraction.split("/")
 
     if (int(Fraction2[0])/int(Fraction2[1]))*100 >= int("99"):
         print ("F")
