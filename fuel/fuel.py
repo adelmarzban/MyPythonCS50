ca0b4a4c3ca118d746800b2898861097d85e5be0
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-15 15:52:20 +0000

    Thu, Dec 15, 2022, 3:52 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,9 +2,9 @@ try:
     Fraction = input("Fraction=")
     Fraction2 = Fraction.split("/")
 
-    if (int(Fraction2[0])/int(Fraction2[1]))*100 == int("100"):
+    if (int(Fraction2[0])/int(Fraction2[1]))*100 >= int("99"):
         print ("F")
-    elif (int(Fraction2[0])/int(Fraction2[1]))*100 == int("0"):
+    elif (int(Fraction2[0])/int(Fraction2[1]))*100 <= int("1"):
         print ("E")
     else: print((int(Fraction2[0]) / int(Fraction2[1])) * 100 )
 
