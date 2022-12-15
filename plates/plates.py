25983d41c8cb77044bed51aebd1a5d4eb7f2786a
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-15 14:55:49 +0000

    Thu, Dec 15, 2022, 2:55 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,10 +1,22 @@
-sym=[ ". ",", ", ";",":" ]
 plate= input("Input: ")
+valid = False
 if len(plate)<=6 and len(plate)>=2 \
             and plate[0].isalpha() \
-            and plate[1].isalpha() \
-            and plate[-4::-1].isalpha() \
-            and plate[0] !="0"  :
+            and plate[1].isalpha():
+    letter_finished = False
+    valid = True
+    for c in plate:
+        if not c.isalpha() and not c.isdigit():
+            valid = False
+        if not letter_finished:
+            if c.isdigit():
+                letter_finished = True
+                if c == '0':
+                    valid = False
+        elif c.isalpha():
+            valid = False
+
+if valid:
     print("Valid")
 else:
     print("Invalid")
