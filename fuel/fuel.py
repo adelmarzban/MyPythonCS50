0a0237e0dbbb7bd8baaa855efee569e335066cb0
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-16 13:42:30 +0000

    Fri, Dec 16, 2022, 1:42 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -4,9 +4,9 @@ while True:
         x0, y0 = Fraction.split ("/")
         x1 = int (x0)
         y1 = int (y0)
-        print (x1, y1)
-        print (type (x1))
-        print (type (x1))
+        #print (x1, y1)
+        #print (type (x1))
+        #print (type (x1))
         if (x1 / y1) * 100 == int ("99") or (x1 / y1) * 100 == int ("100"):
             print ("F")
         elif (x1 / y1) * 100 <= int ("1"):
