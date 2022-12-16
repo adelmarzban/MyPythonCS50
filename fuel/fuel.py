3e01c080000bf41300d9e3ef048dac00916ab387
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-16 11:32:55 +0000

    Fri, Dec 16, 2022, 11:32 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,8 @@
+Fraction = input ("Fraction=")
+x, y = Fraction.split ("/")
+
 try:
-    Fraction = input("Fraction=")
-    x , y = Fraction.split("/")
-    if x.isnumeric and y.isnumeric and x <= y and y > "0" :
+    if x.isnumeric and y.isnumeric and x <= y:
         x=int(x)
         y=int(y)
         if (x/y)*100 == int("99") or (x/y)*100 == int("100"):
@@ -11,6 +12,7 @@ try:
         else:
             print(str(round(x / y * 100))+"%")
 
-
-except :
-    print("")
+except ValueError:
+    print("ValueError")
+except ZeroDivisionError:
+    print("ZeroDivisionError")
