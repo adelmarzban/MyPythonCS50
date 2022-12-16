1f83e1e69e3d3064ddee5971c97bc888af93ead3
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-16 20:36:00 +0000

    Fri, Dec 16, 2022, 8:36 PM +00:00

---
+++ grocery/grocery.py
@@ -0,0 +1,16 @@
+list = {}
+
+while True:
+    try:
+        fruit = input("fruit name")
+        fruit=fruit.title()
+        if fruit in list:
+            list[fruit.title()]+=1
+        else:
+            list[fruit.title()]=1
+
+
+    except(EOFError):
+        for i in list:
+            print(list[i], i)
+        break
