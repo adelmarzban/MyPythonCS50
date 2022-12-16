63d0f34678f5cb7c72de2b58efaaea98be28bd0b
Adel Marzban <93549987+adelmarzban@users.noreply.github.com>
2022-12-16 17:42:33 +0000

    Fri, Dec 16, 2022, 5:42 PM +00:00

---
+++ taqueria/taqueria.py
@@ -0,0 +1,15 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+while True:
+    order = input("order? ")
+    if order.title() in menu:
+        print("$%.2f" % (menu[order.title()]))
